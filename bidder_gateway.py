c47b27f48f709d36f9054c4c3f959dce9954648e
Nicolas Emiliani <nicolas.emiliani@usmediaconsulting.com>
2013-07-23 16:47:09 -0300

    FIX : the key stored by the agent configuration service is a concatenation of the bidder name passed and the pid for for process

--- bidder_gateway.py
+++ bidder_gateway.py
@@ -139,7 +139,7 @@ def start_bidder(name):
     # is a concatenation of the bidder name passed and the
     # pid for for process 
     bidders[name]['agent_conf_name'] = \
-        '%s_%s' % (bidders[name]['executable'], bidders[name]['pid'])
+        '%s_%s' % (name, bidders[name]['pid'])
     logger.info('bidder %s got pid %d' % (name, bidders[name]['pid']))
     
     # great, let's pickle the data
